03bfd5a98f4ec1bef0d7b7246f6fda95e17fd587
Yuliya Volkova <Yuliyavolkova@gmail.com>
2018-03-21 21:22:12 +0300

    изменила стилизацию страницы блог и инпутов авторизации_+_подключила и стилизовала GPP_для_добавления сниппетов кода в блог

--- src/app/blog.js
+++ src/app/blog.js
@@ -3,3 +3,18 @@ import 'normalize.css';
 import '../assets/styles/blog.main.scss';
 import svg4everybody from 'svg4everybody';
 svg4everybody();
+window.onload = function() {runCodePrettify();};
+
+function runCodePrettify() {
+  const pres = document.body.querySelectorAll('pre');
+  [].forEach.call(pres, function(el) {
+    el.classList.add('prettyprint');
+    el.classList.add('linenums');
+  });
+  let script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.async = true;
+
+  script.src = 'https://cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js';
+  (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(script);
+}
